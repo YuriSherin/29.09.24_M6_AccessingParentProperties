efaa33e6c4d403c5967f623c432d2408b20bf925
YuriSherin <YuriSherin@mail.ru>
2024-09-29 23:34:07 +0700

    Change 29/09/24 Add attributes in base class,  method convert_color and change method set_color

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
+from types import NoneType
+
+
 class Vehicle:
     """Базовый класс Транспорт"""
     __COLOR_VARIANTS = ['blue', 'red', 'green', 'black', 'white']   # список допустимых цветов транспорта
+    owner = None
+    __model = None
+    __color = None
+    __engine_power = None
+
 
     def __init__(self, owner, model, color, engine):
         self.owner = owner              # владелец транспорта
         self.__model = model            # модель (марка) транспорта
         self.__engine_power = engine    # мощность двигателя
         self.__color = color            # цвет транспорта
+        self.convert_color()            # приведем допустимые цвета к нижнему регистру
+
+    def convert_color(self):
+        """Метод приводит названия допустимых цветов к нижнему регистру"""
+        i = 0
+        while i < len(self.__COLOR_VARIANTS):
+            self.__COLOR_VARIANTS[i] = self.__COLOR_VARIANTS[i].upper()
+            i += 1
 
     def get_model(self):
         """Метод возвращает модель транспорта"""
@@ -30,7 +46,7 @@ class Vehicle:
     def set_color(self, new_color):
         """Метод устанавливает новый цвет транспорта при условии,
         что новый свет содержится в списке допустимых цветов"""
-        if new_color.lower() in self.__COLOR_VARIANTS:
+        if new_color.upper() in self.__COLOR_VARIANTS:
             self.__color = new_color
         else:
             print(f'Нельзя сменить цвет на {new_color}')
@@ -51,3 +67,19 @@ if __name__ == '__main__':
     vehicle1.owner = 'Vasyok'  # смена владельца транспортного средства
 
     vehicle1.print_info()  # Выводим в консоль, что поменялось
+
+
+# Здравствуйте, Юрий!
+# Хорошее решение, однако есть несколько недочетов, которые следует исправить:
+# 1. В методе `set_color` нужно проверять значение в списке __COLOR_VARIANTS без учета регистра.
+# Для этого сначала следует конвертировать __COLOR_VARIANTS в нижний (или верхний) регистр.
+
+
+# 2. Атрибуты owner, __model, __engine_power и __color следует явно задокументировать как атрибуты объекта,
+# чтобы повысить читаемость кода.
+
+# 3. Атрибут класса __COLOR_VARIANTS должен быть доступен внутри метода путем обращения через self.
+
+# Пожалуйста, проверьте эти моменты и внесите соответствующие исправления.
+# C уважением,
+# Артём
